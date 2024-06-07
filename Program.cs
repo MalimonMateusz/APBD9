1d012ebd2562b609c1fe6936c7b01b08cdddc462
MalimonMateusz <mateuszmalimon1337@gmail.com>
2024-06-08 01:36:20 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using APBD9.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApbdContext>(
+    opttions => opttions.UseSqlServer("Name=ConnectionStrings:Default"));
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
